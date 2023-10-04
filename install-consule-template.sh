671d69cc72cf000b3ea5f17067f377e271ae23b6
mr-maxo <maxo.webmaster@gmail.com>
2023-10-04 12:48:55 +0200

    install requirements for consul-template installation

--- install-consule-template.sh
+++ install-consule-template.sh
@@ -5,6 +5,9 @@ version='0.33.0'
 hc_url='https://releases.hashicorp.com'
 arch='amd64'
 
+apt update
+apt install -y curl unzip
+
 curl -o /tmp/${product}.zip \
     ${hc_url}/${product}/${version}/${product}_${version}_linux_${arch}.zip
 unzip /tmp/${product}.zip -d /usr/bin/
